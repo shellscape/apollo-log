44376c41f7fdf7340cb76232c39ece3935780007
Arjan Singh <asingh@wearfigs.com>
2020-04-08 08:53:05 -0700

    feat: add Typescript types

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+declare module "apollo-log" {
+  type LogLevel = "trace" | "debug" | "info" | "warn" | "error" | "silent";
+
+  export interface LoggerOptions {
+    level?: LogLevel;
+    mutate?: (level: LogLevel, data: Record<string, string> | string) => void;
+    prefix: string;
+    timestamp: boolean;
+  }
+
+  export class ApolloLogExtension {
+    constructor(options: LoggerOptions);
+  }
+}
